ef5a4c0fec34cb6962e5108a8099cf13c94a21cd
FuriousSpongeBob <101704251+FuriousSpongeBob@users.noreply.github.com>
2022-03-17 19:30:46 +0800

    iterative DFS

---
+++ DepthFirstSearch_iter.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+#define ParentNodesNum 6
+#define NodesNum 10
+using namespace std;
+vector<int> visited(NodesNum,0);
+vector<vector<int> > graph;
+/*
+3 2 3 4
+1 5
+2 6 7
+1 8
+1 9
+1 10
+*/
+void DFS(int beg){
+	vector<int>::iterator it;
+	visited[beg]=1;
+	stack<int> s;
+	s.push(beg);
+	cout<<beg<<" - ";
+	while(!s.empty()){
+		beg=s.top();
+		s.pop();
+		if(!visited[beg]){
+			visited[beg]=1;
+			cout<<beg<<" - ";
+		}
+		for(it=graph[beg].begin();it!=graph[beg].end();it++){
+			if(!visited[*it]){
+				s.push(*it);
+			}
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	graph.push_back(vector<int>(1,0));//第0行填充
+	for(int i=1;i<=ParentNodesNum;i++){
+		int edgeNum,temp;
+		cin>>edgeNum;
+		graph.push_back(vector<int>(edgeNum,0));
+		for(int j=0;j<edgeNum;j++){
+			cin>>temp;
+			graph[i][j]=temp;
+		}
+	}
+	DFS(1);
+	return 0;
+}
